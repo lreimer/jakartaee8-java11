6172d1ba6282ae740ac423aba6d0993b6466df24
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2020-01-23 19:49:40 +0100

    Adjusted ALB parameters.

--- fargate/alb.tf
+++ fargate/alb.tf
@@ -12,13 +12,13 @@ resource "aws_alb_target_group" "app" {
   target_type = "ip"
 
   health_check {
-    healthy_threshold = "3"
+    healthy_threshold = "1"
     interval = "30"
     protocol = "HTTP"
     matcher = "200"
-    timeout = "3"
+    timeout = "5"
     path = var.health_check_path
-    unhealthy_threshold = "2"
+    unhealthy_threshold = "3"
   }
 }
 
